dc6c3c705a5395335fb49b9b92e9d6faed151a53
bmsurber <bmsurber@gmail.com>
2017-12-14 20:53:22 -0500

    Make setup.sh actually work

--- setup.sh
+++ setup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 pkgs='vim git stow mc keepassx redshift-gtk vinagre python3-venv python3-tk python3-pip ipython3 ipdb awscli lvm2 cryptsetup p7zip-full cifs-utils'
 virt='virtualbox virtualbox-ext-pack vagrant'
-i3='i3 lxappearance feh compton'
+i3='i3 lxappearance feh compton j4-dmenu-desktop'
 srv='lamp-server^ phpmyadmin libapache2-mod-wsgi-py3'
 
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install $pkgs $virt $i3 $srv
+
 cd ~
-mv .bash_aliases .bash_aliases.old
 git clone https://github.com/bmsurber/dotfiles
+
 cd dotfiles
 git submodule update --init
 git submodule foreach git pull origin master
 git submodule foreach git checkout master
-stow *
+
+for pkg in *; do
+    if [ -d "$pkg" ]; then
+        stow -v "$pkg"
+    fi
+done
+
 cd ~
 source .bash_aliases
