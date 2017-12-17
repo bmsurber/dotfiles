e3824e676473e4760d8424d29621c8513ce398f3
bmsurber <16054900+bmsurber@users.noreply.github.com>
2017-12-16 21:39:01 -0500

    Tidy setup.sh and add comments

--- setup.sh
+++ setup.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-pkgs='vim git stow mc keepassx redshift-gtk vinagre python3-venv python3-tk python3-pip ipython3 ipdb awscli lvm2 cryptsetup p7zip-full cifs-utils'
+
+### Lists of packages to be installed ###
+pkgs='vim git stow mc python3-venv python3-pip ipython3 awscli lvm2 cryptsetup p7zip-full cifs-utils'
+gui='keepassx redshift-gtk vinagre python3-tk xubuntu-restricted-extras'
 virt='virtualbox virtualbox-ext-pack vagrant'
 i3='i3 lxappearance feh compton j4-dmenu-desktop'
 srv='lamp-server^ phpmyadmin libapache2-mod-wsgi-py3'
 
 sudo apt update
-sudo apt -y upgrade
-sudo apt -y install $pkgs $virt $i3 $srv
+sudo apt -y install $pkgs $gui $i3 $virt $srv
 
+
+### Pull down dotfiles repo ###
 cd ~
 git clone https://github.com/bmsurber/dotfiles
 
+
+### Initialize submodules (Vim plugins) ###
 cd dotfiles
 git submodule update --init
-git submodule foreach git pull origin master
-git submodule foreach git checkout master
 
+# Uncomment the next two lines to update vim plugins to the latest versions ###
+# git submodule foreach git pull origin master
+# git submodule foreach git checkout master
+
+
+### Create symlinks in home ###
 for pkg in *; do
     if [ -d "$pkg" ]; then
         stow -v "$pkg"
