9424c4f0ab46924264f9ad4d31d37b405eb62a4d
bmsurber <16054900+bmsurber@users.noreply.github.com>
2018-08-20 23:15:36 -0400

    Remove unneeded autostart items from i3 config

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 pkgs='vim tmux git stow mc python3-venv python3-pip awscli lvm2 cryptsetup oathtool p7zip-full cifs-utils'
 gui='keepassx redshift-gtk vinagre python3-tk xubuntu-restricted-extras'
 virt='virtualbox virtualbox-ext-pack vagrant'
-i3='i3 lxappearance feh compton j4-dmenu-desktop'
+i3='i3 lxappearance feh compton rofi arandr'
 srv='lamp-server^ phpmyadmin libapache2-mod-wsgi-py3'
 
 sudo apt update
