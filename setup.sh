3152c6f4730b9d6bfa717bcae89e7bf286c8765f
bmsurber <16054900+bmsurber@users.noreply.github.com>
2023-04-17 23:03:44 -0400

    Update setup.sh to finish Vim tidying

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 ### Lists of packages to be installed ###
 pkgs='vim tmux git stow mc python3-venv python3-pip awscli lvm2 cryptsetup oathtool p7zip-full cifs-utils'
-gui='keepassx redshift-gtk vinagre python3-tk xubuntu-restricted-extras'
+gui='keepassxc redshift-gtk vinagre python3-tk ubuntu-restricted-extras'
 virt='virtualbox virtualbox-ext-pack vagrant'
 i3='i3 lxappearance feh compton rofi arandr xdotool'
 srv='lamp-server^ phpmyadmin libapache2-mod-wsgi-py3'
@@ -16,13 +16,10 @@ cd ~
 git clone https://github.com/bmsurber/dotfiles
 
 
-### Initialize submodules (Vim plugins) ###
-cd dotfiles
-git submodule update --init
-
-# Uncomment the next two lines to update vim plugins to the latest versions ###
-# git submodule foreach git pull origin master
-# git submodule foreach git checkout master
+### Intall Vim plugins ###
+cd ~/dotfiles/vim/.vim/pack/plugins/start
+git clone https://github.com/bmsurber/gruvbox
+git clone https://github.com/bmsurber/emmet-vim
 
 
 ### Create symlinks in home ###
